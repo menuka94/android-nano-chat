0910addf462a8652ce3314bb6d9cc79290ae0fba
Menuka <menuka.14@cse.mrt.ac.lk>
2017-01-24 23:42:31 +0530

    Fix Firebase Configuration

--- app/src/main/java/com/android/menuka/firebasedemo_bygoogle/MainActivity.java
+++ app/src/main/java/com/android/menuka/firebasedemo_bygoogle/MainActivity.java
@@ -6,7 +6,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseApp app;
+    private FirebaseDatabase database;
+    private FirebaseAuth auth;
+    private FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
         Button btnSend = (Button) findViewById(R.id.btnSend);
         EditText txtMessage = (EditText) findViewById(R.id.txtMessage);
         ListView listViewMessages = (ListView) findViewById(R.id.listViewMessages);
+
+//        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReferenceFromUrl("https://android-nano-chat.firebaseio.com/");
+
+
+
     }
 }
